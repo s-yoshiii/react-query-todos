22d7ac97e3985ca7e5d1d3e2770562b4f7e9eb90
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-02-19 00:49:48 +0900

    TaskEdit（後半）

--- src/components/MainTask.tsx
+++ src/components/MainTask.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ChevronDoubleRightIcon } from '@heroicons/react/solid'
 import TaskList from './TaskList'
+import TaskEdit from './TaskEdit'
 
 const MainTask: FC = () => {
   const navigate = useNavigate()
@@ -19,6 +20,7 @@ const MainTask: FC = () => {
       <p className="mb-10 text-xl font-bold">Tasks</p>
       <div className="grid grid-cols-2 gap-40">
         <TaskList />
+        <TaskEdit />
       </div>
       <ChevronDoubleRightIcon
         className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
--- src/components/TaskEdit.tsx
+++ src/components/TaskEdit.tsx
@@ -5,17 +5,62 @@ import { useQueryTags } from '../hooks/useQueryTags'
 import { useMutateTasks } from '../hooks/useMutateTasks'
 
 const TaskEdit: FC = () => {
-  const editTask = useAppSelector(selectTask)
+  const editedTask = useAppSelector(selectTask)
+  const dispatch = useAppDispatch()
   const { status, data } = useQueryTags()
   const { createTaskMutation, updateTaskMutation } = useMutateTasks()
   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    if (editTask.id === 0) createTaskMutation.mutate(editTask)
+    if (editedTask.id === 0) createTaskMutation.mutate(editedTask)
     else {
-      updateTaskMutation.mutate(editTask)
+      updateTaskMutation.mutate(editedTask)
     }
   }
-  return <div></div>
+  const tagOption = data?.map((tag) => (
+    <option key={tag.id} value={tag.id}>
+      {tag.name}
+    </option>
+  ))
+  console.log('rendered TaskEdit')
+  if (status === 'loading') return <div>{'Loading...'}</div>
+  if (status === 'error') return <div>{'error...'}</div>
+  if (updateTaskMutation.isLoading) {
+    return <span>Updating...</span>
+  }
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          className="mb-3 px-3 py-2 border border-gray-300"
+          placeholder="new task??"
+          onChange={(e) => {
+            dispatch(setEditedTask({ ...editedTask, title: e.target.value }))
+          }}
+          value={editedTask.title}
+        />
+        <button
+          type="submit"
+          className="disabled:opacity-40 my-3 mx-3 py-2 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded"
+          disabled={!editedTask.title || !editedTask.tag}
+        >
+          {editedTask.id === 0 ? 'Create' : 'Update'}
+        </button>
+      </form>
+      <select
+        className="mb-3 px-3 py-2 border border-gray-300"
+        value={editedTask.tag}
+        onChange={(e) => {
+          dispatch(
+            setEditedTask({ ...editedTask, tag: parseInt(e.target.value) })
+          )
+        }}
+      >
+        <option value={0}>Tag</option>
+        {tagOption}
+      </select>
+    </div>
+  )
 }
 
 export default TaskEdit
